f31a46abe7f58a2a578cbeac4c3ac54d84551d0e
Alex <AlexFlowers@gmail.com.>
2025-02-13 21:02:39 -0500

    Added load tasks utilizing SQLite, Python

--- python_pipeline.py
+++ python_pipeline.py
@@ -1,19 +1,16 @@
 import pandas as pd
+import sqlite3
 from pathlib import Path
 
 from datetime import datetime, timedelta
 from airflow.utils.dates import days_ago
 
 from airflow import DAG
+
 from airflow.operators.python import PythonOperator
+from airflow.operators.sqlite_operator import SqliteOperator
 
-default_args = {
-    'owner' : 'Alex'
-}
-
-# Python Functions
-
-data_directory = Path(__file__).parent / "data_export"
+data_directory = Path(__file__).parent / 'data_export'
 data_directory.mkdir(exist_ok=True)
 
 def extract_data():
@@ -25,7 +22,8 @@ def remove_null_values(ti):
     json_data = ti.xcom_pull(task_ids='extract_data')
     
     df = pd.read_json(json_data)
-    df = df.dropna()
+    df = df.dropna(how='all')
+    df.reset_index(inplace=True,drop=True)
 
     print(df)
 
@@ -55,15 +53,27 @@ def groupby_region(ti):
 
     regions_df.to_csv(data_directory/'grouped_by_region.csv', index=False)
 
+def insert_values(ti):
+    json_data = ti.xcom_pull(task_ids='remove_null_values')
+    df = pd.read_json(json_data)
+    
+    conn = sqlite3.connect(Path(__file__).parent / 'database/ins_sqlite.db')
+
+    df.to_sql('insurance_charges',conn,if_exists='append',index_label='id')
+
 # DAG Implementation
 
+default_args = {
+    'owner' : 'Alex'
+}
+
 with DAG(
-    dag_id = 'python_pipeline_insurance_ET',
-    description = 'Insurance dataset extract / transform',
+    dag_id = 'python_pipeline_insurance_ETL',
+    description = 'Insurance dataset ETL utilizing Python / SQL',
     default_args = default_args,
     start_date = days_ago(1),
     schedule_interval = None,
-    tags = ['python', 'extract', 'transform', 'pipeline']
+    tags = ['python', 'SQL', 'extract', 'transform', 'load', 'pipeline']
 ) as dag:
     
     extract_data = PythonOperator(
@@ -86,4 +96,37 @@ with DAG(
         python_callable=groupby_region
     )
 
-extract_data >> remove_null_values >> [groupby_smoker, groupby_region]
+    create_table = SqliteOperator(
+        # This actually should be completely removed, and allow pd to handle the schema,
+        # but leaving just so I can have the quick refresher on SQL in Airflow
+        task_id = 'create_table',
+        sqlite_conn_id = 'ins_sqlite_database',
+        sql = r'''
+            CREATE TABLE IF NOT EXISTS insurance_charges (
+                id          INTEGER PRIMARY KEY,
+                age         INTEGER,
+                sex         TEXT CHECK( sex IN ('male','female') ),
+                bmi         REAL,
+                children    INTEGER,
+                smoker      TEXT CHECK( smoker IN ('yes','no') ),
+                region      TEXT CHECK( region IN ('southwest', 'southeast', 'northwest', 'northeast') ),
+                charges     REAL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        '''
+    )
+
+    insert_values = PythonOperator(
+        task_id = 'insert_values',
+        python_callable = insert_values,
+    )
+
+    display_result = SqliteOperator(
+        task_id = 'display_result',
+        sqlite_conn_id = 'ins_sqlite_database',
+        sql= r'''SELECT * FROM insurance_charges''',
+        do_xcom_push = True
+    )
+
+extract_data >> remove_null_values >> [groupby_smoker, groupby_region, create_table]
+create_table >> insert_values >> display_result
